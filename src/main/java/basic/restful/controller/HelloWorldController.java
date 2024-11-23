d9f1be14634f9d0491638bc2d55569bb933b2745
YongBum <dt10002@naver.com>
2024-11-23 21:29:13 +0900

    [Feat] Controller 사용 및 Json 객체 반환
    [Section 2-6]
    HelloWorld Bean 추가
    - GetMapping 사용하여 path 입력 시 Json 객체 출력

--- src/main/java/basic/restful/controller/HelloWorldController.java
+++ src/main/java/basic/restful/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package basic.restful.controller;
 
+import basic.restful.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 
 }
