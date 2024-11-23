50a6f57050a96d3d25f9f9cf2e0202080224f625
YongBum <dt10002@naver.com>
2024-11-23 21:17:35 +0900

    [Feat] Controller 생성 및 GetMapping 사용
    [Section 2-5]
    HelloWorld Controller 추가
    - GetMapping 사용하여 path 입력 시 문자열 출력

--- src/main/java/basic/restful/RestfulApplication.java
+++ src/main/java/basic/restful/RestfulApplication.java
@@ -2,17 +2,12 @@ package basic.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
 public class RestfulApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ac = SpringApplication.run(RestfulApplication.class, args);
-        String[] beanNames = ac.getBeanDefinitionNames();
-
-        for (String beanName : beanNames)
-            System.out.println(beanName);
+        SpringApplication.run(RestfulApplication.class, args);
     }
 }
