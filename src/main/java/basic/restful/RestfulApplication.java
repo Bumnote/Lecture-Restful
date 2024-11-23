888f1bcac8ef88b3d686b10094864ce1d645c2d8
YongBum <dt10002@naver.com>
2024-11-23 21:11:20 +0900

    [Feat] SpringContext 포함된 다양한 Bean 확인
    [Section 2-4]
    Spring Boot Project 구조 확인과 실행 방법
    - ApplicationContext 사용

--- src/main/java/basic/restful/RestfulApplication.java
+++ src/main/java/basic/restful/RestfulApplication.java
@@ -2,12 +2,17 @@ package basic.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
 public class RestfulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestfulApplication.class, args);
+        ApplicationContext ac = SpringApplication.run(RestfulApplication.class, args);
+        String[] beanNames = ac.getBeanDefinitionNames();
+
+        for (String beanName : beanNames)
+            System.out.println(beanName);
     }
 }
