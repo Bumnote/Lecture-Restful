a155d8ac981fbd3fe5574acd64be87e190c65068
YongBum <dt10002@naver.com>
2024-11-23 21:03:07 +0900

    Project Setting

---
+++ src/test/java/basic/restful/RestfulApplicationTests.java
@@ -0,0 +1,15 @@
+package basic.restful;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+    @Test
+    @DisplayName("기본 테스트 생성자")
+    void contextLoads() {
+    }
+
+}
