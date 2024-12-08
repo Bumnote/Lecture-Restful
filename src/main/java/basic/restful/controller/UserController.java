0d3891cefcca18e91a40cd1570875d26ece8cd61
YongBum <dt10002@naver.com>
2024-12-08 22:35:03 +0900

    [Feat] HTTP Status Code 제어
    [Section 3-4]
    HTTP Status Code 제어
    - ServletUriComponentsBuilder 활용
    - ResponseEntity 활용

--- src/main/java/basic/restful/controller/UserController.java
+++ src/main/java/basic/restful/controller/UserController.java
@@ -2,8 +2,11 @@ package basic.restful.controller;
 
 import basic.restful.bean.User;
 import basic.restful.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,8 +29,14 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 
