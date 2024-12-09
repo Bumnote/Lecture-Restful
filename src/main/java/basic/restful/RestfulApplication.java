423a23077c2652fe42652f100286c0fda8ed7f46
YongBum <dt10002@naver.com>
2024-12-10 01:29:53 +0900

    [Feat] 다국어 처리를 위한 Internationalization 구현 방법
    [Section 4-3]
    다국어 처리를 위한 Internationalization 구현 방법
    - LocaleResolver 개념
    - MessageSource 개념
    - RequestHeader 통해서 언어 설정

--- src/main/java/basic/restful/RestfulApplication.java
+++ src/main/java/basic/restful/RestfulApplication.java
@@ -2,10 +2,22 @@ package basic.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 
 @SpringBootApplication
 public class RestfulApplication {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulApplication.class, args);
