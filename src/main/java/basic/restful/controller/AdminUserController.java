a381adce3840bacb080675fb10db157491910c18
YongBum <dt10002@naver.com>
2024-12-11 01:19:53 +0900

    [Feat] Version 관리 - Parameter와 Header를 이용한 버전관리
    [Section 4-9]
    Version 관리 - Parameter와 Header를 이용한 버전관리
    - URI Versioning
    - Request Parameter Versioning
    - (Custom) Headers Versioning
    - Media Type Versioning

--- src/main/java/basic/restful/controller/AdminUserController.java
+++ src/main/java/basic/restful/controller/AdminUserController.java
@@ -47,7 +47,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/v1/users/{id}")
+    //@GetMapping("/v1/users/{id}")
+    //@GetMapping(value = "/users/{id}", params = "version=1")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
     public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
 
         User user = service.findOne(id);
@@ -67,7 +70,10 @@ public class AdminUserController {
         return mapping;
     }
 
-    @GetMapping("/v2/users/{id}")
+    //@GetMapping("/v2/users/{id}")
+    //@GetMapping(value = "/users/{id}", params = "version=2")
+    //@GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
     public MappingJacksonValue retrieveUser4AdminV2(@PathVariable int id) {
 
         User user = service.findOne(id);
